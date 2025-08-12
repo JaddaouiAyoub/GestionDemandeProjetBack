d6aada953a08e4dc038c1c674402c1b33f36af38
JaddaouiAyoub <jaddaoui.salim@gmail.com>
2025-08-12 10:13:28 +0100

    Traitement des dossiers d'exécution

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/demandes', demandeRoutes);
 app.use('/api/dossierEtude',dossierEtudeRoutes);
+app.use('/api/dossierExecution',dossierExecutionRoutes);
 // 🔥 Ligne à ajouter pour servir le dossier uploads correctement
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
